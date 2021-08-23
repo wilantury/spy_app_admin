4495ca02f64f998a2e476a215455ff04b0e940aa
Wilson Antury <ventas@antury.net>
2021-08-23 18:08:48 -0500

    [ADD] apps to config file

--- src/spy_dashboard_prj/settings.py
+++ src/spy_dashboard_prj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local Apps
+    'spy_app',
+    'auth_app'
 ]
 
 MIDDLEWARE = [
